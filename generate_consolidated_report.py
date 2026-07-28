c9625a925733919d9a3a8bf6b7c4ceb53d4805d5
Maksim Anisimov <46654281+m9117882424@users.noreply.github.com>
2026-07-28 11:10:47 +0300

    feat: add consolidated report entrypoint

---
+++ generate_consolidated_report.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Standalone entrypoint for the consolidated vehicle report."""
+
+from consolidated_report import main
+
+
+if __name__ == "__main__":
+    main()
